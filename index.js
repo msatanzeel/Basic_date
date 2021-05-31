7b95a68e23af040990073af020a11dd322a953a7
msatanzeel <74665170+msatanzeel@users.noreply.github.com>
2021-05-31 17:45:31 +0530

    Updated the wrong code the  the new one

--- index.js
+++ index.js
@@ -1,112 +1,25 @@
-var add=document.querySelector("#add");
+ var a;
+ var time;
+ var date;
 
-var tit;
-var desc;
+ 
 
-// update();
-
-// localStorage.setItem("Json_items",JSON.stringify(""));
-
-
-function update()
+function set_date()
 {
-        console.log("updating the list...");
-    
-        tit=document.getElementById("title").value;
-    
-        desc=document.querySelector("#description").value;
+    options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    a=new Date();
+    date=a.toLocaleDateString(undefined,options);
+    // time=a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+    time=a.toLocaleTimeString('en-US');
+    document.getElementById("id1").innerHTML=time + "<br>" + " on " + date;
 
-        console.log(tit,desc);
-
-        var arr=[];
-        
-        
-        // console.log(document.getElementById("title").innerHTML);
-
-        if(localStorage.getItem("Json_items")==null && (tit && desc))
-        {
-            arr.push([tit,desc]);
-            localStorage.setItem('Json_items',JSON.stringify(arr));
-        }
-        else if(tit && desc)
-        {
-            JsonStr=localStorage.getItem('Json_items');
-            arr=JSON.parse(JsonStr);
-            arr.push([tit,desc]);
-            localStorage.setItem('Json_items',JSON.stringify(arr));  
-        }
-
-        JsonStr=localStorage.getItem('Json_items');
-        arr=JSON.parse(JsonStr);
-        localStorage.setItem('Json_items',JSON.stringify(arr)); 
-        console.log(arr); 
-
-        
-
-        document.getElementById("title").value='';
-        document.getElementById("description").value='';
-
-        
-    
-        // populate the table
-    
-        
-        var tbody=document.getElementById("tbody");
-        str="";
-
-            for(let i=0;i<arr.length;i++)
-            {
-                str+=`
-                <tr>
-                <th scope="row">${i+1}</th>
-                <td>${arr[i][0]}</td>
-                <td>${arr[i][1]}</td>
-                <td><button type="button" class="btn btm-sm btn-secondary" onclick=deleted(${i})>Delete</button></td>
-                </tr>`;
-            
-            }
-            console.log(str);
-    
-            tbody.innerHTML=str;
-            // console.log("line 70");
-
-        
-        
-        
-        
 }
 
-update();
-add.addEventListener("click",update);
-
-
-//deleting the element
-
-
-function deleted(ind)
-{
-    console.log("deleting the item");
-    JsonStr=localStorage.getItem('Json_items');
-    arr=JSON.parse(JsonStr);
-    arr.splice(ind,1);
-    localStorage.setItem("Json_items",JSON.stringify(arr));
-    update();
-}
-
-var clr=document.getElementById('clear');
-
-clr.addEventListener("click",()=>{
-    console.log("deleting the item");
-    JsonStr=localStorage.getItem('Json_items');
-    arr=JSON.parse(JsonStr);
-    arr.splice(0,arr.length);
-    localStorage.setItem("Json_items",JSON.stringify(arr));
-    update();
-});
+setInterval("set_date()",1000);
 
 
 
 
 
 
-
+// tem.innerHTML=time;
